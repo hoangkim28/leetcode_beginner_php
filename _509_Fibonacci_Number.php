c1cc38417e97a6dff3cb6c24278e931646ed9165
hoangkim28 <hoangkim28@outlook.com>
2022-01-30 13:17:20 +0700

    Solution 2 - 509. Fibonacci Number

--- _509_Fibonacci_Number.php
+++ _509_Fibonacci_Number.php
@@ -1,13 +1,36 @@
 <?php
-class Solution {
+class Solution
+{
 
     /**
      * @param Integer $n
      * @return Integer
      */
-    function fib($n) {
-        if($n <= 1)
+    public function fib($n)
+    {
+        if ($n <= 1) {
             return $n;
-        return ($this->fib($n-1) + $this->fib($n-2));
+        }
+
+        return ($this->fib($n - 1) + $this->fib($n - 2));
+    }
+    public function fib2($n)
+    {
+        if ($n == 0) {
+            return 0;
+        }
+        if ($n == 1 || $n == 2) {
+            return 1;
+        }
+        $n1 = 1;
+        $n2 = 1;
+        $result = "";
+        for ($i = 3; $i <= $n; $i++) {
+            $result = $n1 + $n2;
+            $n1 = $n2;
+            $n2 = $result;
+        }
+        return $result;
+
     }
 }
